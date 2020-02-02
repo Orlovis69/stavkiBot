3e508829761c74ecf3d9a2def8bd573fc404922d
Evan Rosenberg <orlovis69@gmail.com>
2020-02-02 23:42:20 +0530

    First commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,160 @@
+"use strict";
+
+import babel from "gulp-babel";
+import browserSync from "browser-sync";
+import gulp from "gulp";
+import cssnano from "gulp-cssnano";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import sass from "gulp-sass";
+import rename from "gulp-rename";
+import stylelint from "gulp-stylelint";
+import purgecss from "gulp-purgecss";
+import newer from "gulp-newer";
+import pump from "pump";
+
+// Project paths
+const paths = {
+  html: {
+    src: "./development/**/*.html"
+  },
+  images: {
+    src: "./development/img/**.*",
+    dest: "./production/img/"
+  },
+  fonts: {
+    src: "./development/fonts/**/**.*",
+    dest: "./production/fonts/"
+  },
+  styles: {
+    src: "./development/**/*.scss",
+    dest: "./production/css/"
+  },
+  scripts: {
+    src: "./development/**/*.js"
+  },
+  php: {
+    src: "./development/server/**.*",
+    dest: "./production/server/"
+  },
+  dest: "./production/"
+};
+
+// Setting browser sync
+const server = browserSync.create();
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: "./production/"
+    }
+  });
+  done();
+}
+
+// HTML copy and paste
+export function html(done) {
+  pump(
+    [gulp.src(paths.html.src), newer(paths.dest), gulp.dest(paths.dest)],
+    done
+  );
+}
+
+// Images copy and paste
+export function images(done) {
+  pump(
+    [
+      gulp.src(paths.images.src),
+      newer(paths.images.dest),
+      gulp.dest(paths.images.dest)
+    ],
+    done
+  );
+}
+
+// Fonts copy and paste
+export function fonts(done) {
+  pump(
+    [
+      gulp.src(paths.fonts.src),
+      newer(paths.fonts.dest),
+      gulp.dest(paths.fonts.dest)
+    ],
+    done
+  );
+}
+
+// CSS copy and paste
+export function styles(done) {
+  pump(
+    [
+      gulp.src(paths.styles.src),
+      newer(paths.dest),
+      sass().on("error", sass.logError),
+      rename("style.css"),
+      // purgecss({
+      //   content: [paths.html.src]
+      // }),
+      stylelint({
+        reporters: [{ formatter: "string", console: true }],
+        failAfterError: false,
+        fix: true
+      }),
+      postcss({}),
+      cssnano(),
+      gulp.dest(paths.styles.dest)
+    ],
+    done
+  );
+}
+
+// JS copy and paste
+export function scripts(done) {
+  pump(
+    [
+      gulp.src(paths.scripts.src),
+      newer(paths.dest),
+      babel(),
+      gulp.dest(paths.dest)
+    ],
+    done
+  );
+}
+
+// PHP copy and paste
+export function php(done) {
+  pump(
+    [gulp.src(paths.php.src), newer(paths.php.dest), gulp.dest(paths.php.dest)],
+    done
+  );
+}
+
+// Watch everything
+export function watch() {
+  gulp.watch(paths.html.src, gulp.series(html, reload));
+  gulp.watch(paths.images.src, gulp.series(images, reload));
+  gulp.watch(paths.fonts.src, gulp.series(fonts, reload));
+  gulp.watch(paths.styles.src, gulp.series(styles, reload));
+  gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
+  gulp.watch(paths.php.src, gulp.series(php, reload));
+}
+
+// Map out sequence of events on first load
+const firstRun = gulp.series(
+  html,
+  images,
+  fonts,
+  styles,
+  scripts,
+  php,
+  serve,
+  watch
+);
+
+// Run the whole thing
+export default firstRun;
